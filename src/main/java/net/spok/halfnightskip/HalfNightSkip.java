e8ed77a90da7666c2315a832f61bef3b49605f67
UwU-Spok <antoine.storme@icloud.com>
2026-07-28 19:15:56 +0200

    first commit

---
+++ src/main/java/net/spok/halfnightskip/HalfNightSkip.java
@@ -0,0 +1,29 @@
+package net.spok.halfnightskip;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+
+
+public class HalfNightSkip implements ModInitializer {
+	public static final String MOD_ID = "half-night-skip";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+
+
+
+
+	}
+
+	
+}
